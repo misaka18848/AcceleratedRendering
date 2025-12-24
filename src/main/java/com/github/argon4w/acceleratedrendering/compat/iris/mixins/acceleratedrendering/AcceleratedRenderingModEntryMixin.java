95aff9a994b19799478301264f667a39d98b0809
御坂18848 <122113416+misaka18848@users.noreply.github.com>
2025-12-24 17:18:58 +0800

    Update AcceleratedRenderingModEntryMixin.java

--- src/main/java/com/github/argon4w/acceleratedrendering/compat/iris/mixins/acceleratedrendering/AcceleratedRenderingModEntryMixin.java
+++ src/main/java/com/github/argon4w/acceleratedrendering/compat/iris/mixins/acceleratedrendering/AcceleratedRenderingModEntryMixin.java
@@ -1,8 +1,7 @@
 package com.github.argon4w.acceleratedrendering.compat.iris.mixins.acceleratedrendering;
 
-import com.github.argon4w.acceleratedrendering.AcceleratedRenderingModEntry;
 import com.github.argon4w.acceleratedrendering.compat.iris.programs.IrisPrograms;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.ModLoadingContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,14 +10,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(AcceleratedRenderingModEntry.class)
 public class AcceleratedRenderingModEntryMixin {
 
-	@Inject(
-			method	= "<init>",
-			at		= @At("TAIL"),
-			remap	= false
-	)
-	public void registerIrisEvents(FMLJavaModLoadingContext context, CallbackInfo	ci) {
-		context
-				.getModEventBus	()
-				.register		(IrisPrograms.class);
-	}
+    @Inject(
+        method = "<init>",
+        at = @At("TAIL"),
+        remap = false
+    )
+    private void registerIrisEvents(CallbackInfo ci) {
+        // 使用静态方法获取当前 mod 的事件总线
+        ModLoadingContext.get()
+            .getActiveContainer()
+            .getEventBus()
+            .register(IrisPrograms.class);
+    }
 }
