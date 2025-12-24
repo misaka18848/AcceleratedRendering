accf68a5297394862b41b3ec10fd022bd54c6775
御坂18848 <122113416+misaka18848@users.noreply.github.com>
2025-12-24 16:05:46 +0800

    Update AcceleratedRenderingModEntry.java

--- src/main/java/com/github/argon4w/acceleratedrendering/AcceleratedRenderingModEntry.java
+++ src/main/java/com/github/argon4w/acceleratedrendering/AcceleratedRenderingModEntry.java
@@ -4,16 +4,17 @@ import com.github.argon4w.acceleratedrendering.configs.FeatureConfig;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.ModLoadingContext; // ← 新增导入
 import org.slf4j.Logger;
 
-@Mod(value= AcceleratedRenderingModEntry.MOD_ID)
+@Mod(value = AcceleratedRenderingModEntry.MOD_ID)
 public class AcceleratedRenderingModEntry {
 
-	public static final String MOD_ID = "acceleratedrendering";
-	public static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "acceleratedrendering";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
-	public AcceleratedRenderingModEntry(FMLJavaModLoadingContext context) {
-		context.registerConfig(ModConfig.Type.CLIENT, FeatureConfig.SPEC);
-	}
+    public AcceleratedRenderingModEntry() {
+        // 使用 ModLoadingContext.get() 而不是 FMLJavaModLoadingContext
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, FeatureConfig.SPEC);
+    }
 }
