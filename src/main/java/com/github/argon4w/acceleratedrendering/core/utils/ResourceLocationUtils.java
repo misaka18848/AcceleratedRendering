ecf89be798a6f5e7ccb954f467d8e3c86dbb7b6d
御坂18848 <122113416+misaka18848@users.noreply.github.com>
2025-12-24 16:05:12 +0800

    Update ResourceLocationUtils.java

--- src/main/java/com/github/argon4w/acceleratedrendering/core/utils/ResourceLocationUtils.java
+++ src/main/java/com/github/argon4w/acceleratedrendering/core/utils/ResourceLocationUtils.java
@@ -1,11 +1,12 @@
-package com.github.argon4w.acceleratedrendering.core.utils;
+ResourceLocationUtils.javapackage com.github.argon4w.acceleratedrendering.core.utils;
 
 import com.github.argon4w.acceleratedrendering.AcceleratedRenderingModEntry;
 import net.minecraft.resources.ResourceLocation;
 
 public class ResourceLocationUtils {
 
-	public static ResourceLocation create(String path) {
-		return ResourceLocation.fromNamespaceAndPath(AcceleratedRenderingModEntry.MOD_ID, path);
-	}
+    public static ResourceLocation create(String path) {
+        // Forge 47.2.19 / Minecraft 1.20.1 不支持 fromNamespaceAndPath
+        return new ResourceLocation(AcceleratedRenderingModEntry.MOD_ID, path);
+    }
 }
