5feeb0e50f37a6778c983b762758a57707da2260
御坂18848 <122113416+misaka18848@users.noreply.github.com>
2025-12-24 20:34:06 +0800

    Update AcceleratedRenderingModEntry.java

--- src/main/java/com/github/argon4w/acceleratedrendering/AcceleratedRenderingModEntry.java
+++ src/main/java/com/github/argon4w/acceleratedrendering/AcceleratedRenderingModEntry.java
@@ -5,6 +5,7 @@ import com.mojang.logging.LogUtils;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.ModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(AcceleratedRenderingModEntry.MOD_ID)
@@ -13,12 +14,8 @@ public class AcceleratedRenderingModEntry {
     public static final String MOD_ID = "acceleratedrendering";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    // 接收 FMLJavaModLoadingContext 参数
     public AcceleratedRenderingModEntry(FMLJavaModLoadingContext context) {
-        // 注册配置
-        context.registerConfig(ModConfig.Type.CLIENT, FeatureConfig.SPEC);
-        
-        // 保存事件总线（供 Mixin 或其他地方使用）
-        // 如果你需要在别处用，可以存为静态字段
+        // 注册配置（通过 ModLoadingContext）
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, FeatureConfig.SPEC);
     }
 }
